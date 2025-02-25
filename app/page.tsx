7fd0283ec6d11c32c434799a4c3bdc0225342f83
Dhruva-Das <dhruva00099@gmail.com>
2025-02-25 18:01:25 +0530

    changed navigation bar

--- app/page.tsx
+++ app/page.tsx
@@ -169,7 +169,7 @@ const Home: React.FC = () => {
           className={`flex items-center justify-between max-w-5xl w-full rounded-full px-8 py-4 transition-all duration-300 border border-gray-700 ${
             isScrolled
               ? "bg-blue-750/85 backdrop-blur-md shadow-lg"
-              : "bg-blue-900/40 backdrop-blur-sm"
+              : "bg-blue-500/70 backdrop-blur-sm"
           }`}
           initial={{ y: -100 }}
           animate={{ y: 0 }}
